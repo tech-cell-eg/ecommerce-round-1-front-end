9d704c6e683db3846e2eb7373af4ee8f8dfcf40f
JohnSamehRoshdy <john.sameh13@gmail.com>
2024-12-15 08:53:48 -0800

    Side Bar 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import Sidebar from "./SideBar";
-
 const App = () => {
   return (
     <div>
-      {/* <h1 className="text-3xl font-bold underline">Hello world!</h1> */}
-      <Sidebar />
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 };
