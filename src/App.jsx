6141ff0becc306c2b5df1862603441328621477e
mazen-essam <mazenessam80@gmail.com>
2024-12-16 12:49:45 +0200

    updating imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,31 @@
-
-import React from 'react'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Layout from './Component/Layout'
-import Login from './Auth/Login/Login'
-import Register from './Auth/Register/Register'
-import ForgetPass from './Auth/ForgetPass/ForgetPass'
-import VerifyOtp from './Auth/VerifyOtp/VerifyOtp'
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./components/Layout";
+import Login from "./Auth/Login/Login";
+import Register from "./Auth/Register/Register";
+import ForgetPass from "./Auth/ForgetPass/ForgetPass";
+import VerifyOtp from "./Auth/VerifyOtp/VerifyOtp";
 import Checkout from "./pages/Checkout";
 import NotFound from "./pages/NotFound";
-import ProductListing from './pages/product-listing/ProductListing'
+import ProductListing from "./pages/product-listing/ProductListing";
 
 export default function App() {
-const routes = createBrowserRouter([
-  {path:"/",element:<Layout/>,children:[
-    {path:"login",element:<Login/>},
-    {path:"register",element:<Register/>},
-    {path:"forgetpassword",element:<ForgetPass/>},
-    {path:"verifyotp",element:<VerifyOtp/>},
-    {path:"/about",element:<h1>About</h1>},
-    { path: "/checkout", element: <Checkout /> },
-    { path: "/viewproducts", element: <ProductListing /> },
-    { path: "*", element: <NotFound /> },
-  ]}
-])
-
+  const routes = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        { path: "login", element: <Login /> },
+        { path: "register", element: <Register /> },
+        { path: "forgetpassword", element: <ForgetPass /> },
+        { path: "verifyotp", element: <VerifyOtp /> },
+        { path: "/about", element: <h1>About</h1> },
+        { path: "/checkout", element: <Checkout /> },
+        { path: "/viewproducts", element: <ProductListing /> },
+        { path: "*", element: <NotFound /> },
+      ],
+    },
+  ]);
 
   return (
     <>
