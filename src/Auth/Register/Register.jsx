a3d1aab42850b306ba7dbd06e66a4bef35f12b44
ayaFarh <farhaya004@gmail.com>
2024-12-17 13:17:27 +0200

    add validation to register

--- src/Auth/Register/Register.jsx
+++ src/Auth/Register/Register.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import { useFormik } from 'formik'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import * as yup from 'yup'
 
 export default function Register() {
+    const [errorMsg,setErrorMessage] = useState(null)
+     
+    const validationSchema =  yup.object({
+        firstName: yup.string().min(7,"min 7 characters").required("required"),
+        lastName: yup.string().min(7,"min 7 characters").required("required"),
+        email:yup.string().required('required').email("write avalid email"),
+        password:yup.string().required('required').matches(/^(?=.*[A-Z]).{8,}$/,'Min 8 characters with at least one uppercase letter'),
+         })
+
+
+    const formik = useFormik({
+        initialValues: {
+            firstName: '',
+            lastName: '',
+            email: '',
+            password: '',
+        },
+        validationSchema,
+        onSubmit: (values) => {
+            setErrorMessage(error)
+            console.log(values)
+        },
+    })
+
   return <>
   <section className='grid grid-cols-12 gap-2 h-screen '>
     
@@ -14,31 +40,31 @@ export default function Register() {
        <h1 className='text-2xl max-[280px]:text-lg font-semibold'>Kirst</h1>
        </div>
     </div>
-    <div className='col-span-12 md:col-span-5 py-6'>
+    <div className='col-span-12 md:col-span-5 md:bg-white bg-gray-100 bg-opacity-80 py-6 max-[766px]:absolute w-full h-full'>
     
-        <form className='flex flex-col justify-center items-start space-y-4 w-[85%] m-auto h-full'>
+        <form className='flex flex-col justify-center items-start space-y-4 w-[85%] m-auto h-full' onSubmit={formik.handleSubmit}>
         <div className='space-y-1'>
-        <h2 className='text-2xl font-bold'>Create an new account</h2>
+        <h2 className='text-2xl font-bold max-[278px]:text-xl'>Create new account</h2>
         <p className='text-gray-400 '>Please enter details</p>
     </div>
 
             <div className='flex flex-col w-full space-y-1'>
-                <label htmlFor='firstName'>First Name</label>
-                <input type='text' id='firstName' className='rounded-lg border-2 border-gray-600 py-2 px-2' required/>
+                <label htmlFor='firstName' className='flex items-center justify-between space-x-2'><span>First Name</span> <span>{formik.errors.firstName && formik.touched.firstName ? (<div className='text-red-600 mt-1 font-semibold text-sm'>{formik.errors.firstName}</div>):('')}</span></label>
+                <input type='text' id='firstName' name='firstName' onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.firstName} className='rounded-lg border-2 bg-transparent border-gray-600 py-2 px-2'/>
             </div>
 
             <div className='flex flex-col w-full space-y-1'>
-                <label htmlFor='LastName'>Last Name</label>
-                <input type='text' id='LastName' className='rounded-lg border-2 border-gray-600 py-2 px-2' required/>
+                <label htmlFor='LastName' className='flex items-center justify-between space-x-2'><span>Last Name</span> <span>{formik.errors.lastName && formik.touched.lastName ? (<div className='text-red-600 mt-1 font-semibold text-sm'>{formik.errors.lastName}</div>):('')}</span></label>
+                <input type='text' id='LastName' name='lastName'  onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.lastName} className='rounded-lg border-2 bg-transparent border-gray-600 py-2 px-2'/>
             </div>
 
             <div className='flex flex-col w-full space-y-1'>
-                <label htmlFor='email'>Email Address</label>
-                <input type='email' id='email' className='rounded-lg border-2 border-gray-600 py-2 px-2' required/>
+                <label htmlFor='email' className='flex items-center justify-between space-x-2'><span className='text-nowrap'>Email Address</span> <span>{formik.errors.email && formik.touched.email ? (<div className='text-red-600 mt-1 font-semibold text-sm'>{formik.errors.email}</div>):('')}</span></label>
+                <input type='email' id='email' className='rounded-lg border-2 bg-transparent border-gray-600 py-2 px-2' name='email'  onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.email} />
             </div>
             <div className='flex flex-col w-full space-y-1'>
-                <label htmlFor='password'>password </label>
-                <input type='password' id='password' className='rounded-lg border-2 border-gray-600 py-2 px-2' required/>
+                <label htmlFor='password' className='flex items-center justify-between space-x-2'><span>password</span> <span>{formik.errors.password && formik.touched.password ? (<div className='text-red-600 mt-1 font-semibold text-sm'>{formik.errors.password}</div>):('')}</span> </label>
+                <input type='password' id='password' className='rounded-lg border-2 bg-transparent border-gray-600 py-2 px-2' name='password'  onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.password}/>
             </div>
     
   <div>
@@ -51,7 +77,7 @@ export default function Register() {
 
 
             <div className='w-full  '>
-                <button type='submit' className='bg-black text-white py-2 rounded-lg w-full '>Signup</button>
+                <button type='submit' className='bg-black text-white py-2 rounded-lg w-full  hover:bg-gray-800 transition-all duration-200'>Signup</button>
             </div>
         </form>
     </div>
