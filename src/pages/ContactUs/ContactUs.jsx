8de20f6d126db9daeac1c8832dcf7fa7d910cb30
ayaFarh <farhaya004@gmail.com>
2024-12-25 14:28:31 +0200

    logic for contact

--- src/pages/ContactUs/ContactUs.jsx
+++ src/pages/ContactUs/ContactUs.jsx
@@ -1,13 +1,16 @@
 import { useFormik } from 'formik'
-import React from 'react'
+import React, { useState } from 'react'
 import { Helmet } from 'react-helmet'
 import { BsTelephone } from 'react-icons/bs'
 import { CiInstagram, CiLocationOn, CiMail } from 'react-icons/ci'
 import { FaFacebookF, FaTwitter, FaYoutube } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 import * as yup from "yup";
+import contact from '../../api/contactUs/contactUs'
+import toast from 'react-hot-toast'
 
 export default function ContactUs() {
+  const [disableBtn,setDisableBtn] =useState(false)
   const validationSchema = yup.object({
     name: yup
     .string()
@@ -16,21 +19,42 @@ export default function ContactUs() {
       .string()
       .required("email is required")
       .email("write avalid email"),
-      message: yup
+      msg: yup
       .string()
-      .required("message is required")
+      .required("message is required"),
+      password: yup
+      .string()
+      .required("password is required")
+      .matches(
+        /^(?=.*[A-Z])(?=.*[a-z]).{8,}$/, 
+        "Min 8 characters with at least one uppercase letter and one lowercase letter"
+      ),
   });
 
   const formik = useFormik({
     initialValues: {
       name: '',
       email: "",
-      message: "",
+      msg: "",
+      password: ""
     },
     validationSchema,
-    onSubmit: async () => {
-      // submit the form
-      console.log(formik.values)
+    onSubmit: async (values) => {
+      let id;
+      setDisableBtn(true); 
+      console.log(values);
+      
+      try {
+        id = toast.loading("Waiting...");
+        const contactus = await contact(values) ;
+        toast.dismiss(id);
+        toast.success("message send successfully");
+      } catch (error) {
+        toast.dismiss(id);
+        toast.error(error.message || "An error occurred during login");
+      } finally {
+        setDisableBtn(false);
+        }
     }
 })
 
@@ -39,7 +63,7 @@ export default function ContactUs() {
     <title>Contact Us</title>
     <meta name="description" content="Contact us for any questions"/>
   </Helmet>
-  <section className='min-h-screen pt-4 pb-10 flex flex-col items-center justify-center bg-[#F5F5F8] relative overflow-hidden'>
+  <section className='min-h-screen pt-4 pb-24 flex flex-col items-center justify-center bg-[#F5F5F8] relative overflow-hidden'>
   <div className="container-main w-full space-y-4 py-12 bg-white shadow-xl rounded-lg flex flex-col items-center justify-center relative z-10  ">
   <h2 className='text-bold text-3xl py-2 border-b w-fit border-black  '>Get in touch</h2>
     <div className="grid grid-cols-1 md:grid-cols-2 gap-10  px-4 w-full py-6 ">
@@ -55,9 +79,22 @@ export default function ContactUs() {
        <input type="email" id='email' name="email" placeholder="Email Address" className='form-control' onChange={formik.handleChange} value={formik.values.email} />
        {formik.touched.email && formik.errors.email && <p className='text-red-500'>{formik.errors.email}</p>}
        </div>
+        <div>
+  <input 
+    type="password" 
+    id='password' 
+    name="password" 
+    placeholder="Your password" 
+    className='form-control' 
+    onChange={formik.handleChange} 
+    value={formik.values.password} 
+  />
+  {formik.touched.password && formik.errors.password && <p className='text-red-500'>{formik.errors.password}</p>}
+</div>
+
        <div>
-        <textarea id='message' name="message" placeholder="Your Message" className='form-control' rows="6"  onChange={formik.handleChange} value={formik.values.message}></textarea>
-        {formik.touched.message && formik.errors.message && <p className='text-red-500'>{formik.errors.message}</p>}
+        <textarea id='msg' name="msg" placeholder="Your Message" className='form-control' rows="6"  onChange={formik.handleChange} value={formik.values.msg}></textarea>
+        {formik.touched.msg && formik.errors.msg && <p className='text-red-500'>{formik.errors.msg}</p>}
        </div>
 
        <button className='btn-primary ' type='submit'>
@@ -66,8 +103,9 @@ export default function ContactUs() {
         </form>
 
         {/* information */}
-        <div className='space-y-4 h-full'>
-            <div className=''>
+        <div className='space-y-4 h-full flex flex-col justify-between'>
+         
+           <div className=''>
             <Link to="https://www.google.com/maps" ><CiLocationOn className='inline-block mr-2  text-xl' />Infomation technologies building, Victoria Island, Lagos, Nigeria.</Link>
             </div>
        <div>
@@ -83,13 +121,14 @@ export default function ContactUs() {
         <Link to={"#"}><FaFacebookF /></Link>
         <Link to={"#"}><FaTwitter /></Link>
        </div>
+           
        {/* map */}
        <div className='h-[50%] bg-rud-300'>
        <iframe
                 src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12096.995586362455!2d-74.000396!3d40.712537!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25a22a3bda30d%3A0xb89d1fe6bc499443!2sDowntown%20Conference%20Center!5e0!3m2!1sen!2sus!4v1705840870375!5m2!1sen!2sus"
-                allowfullscreen="" loading="lazy"
+                loading="lazy"
                 className='w-full h-full '
-                referrerpolicy="no-referrer-when-downgrade"></iframe>
+                ></iframe>
        </div>
         </div>
     </div>
