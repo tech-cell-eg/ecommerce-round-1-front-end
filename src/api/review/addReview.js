dbd6fc6490eb37668ac14b0a0e48971f3606a97b
ayaFarh <farhaya004@gmail.com>
2024-12-26 21:33:33 +0200

    add review

--- src/api/review/addReview.js
+++ src/api/review/addReview.js
@@ -3,14 +3,13 @@ import api, { handleError } from "../axiosConfig";
 
 const addReview = async (values) => {
     const token = localStorage.getItem("token");
-    const user_id = useSelector(state => state.user.id);
 
   try {
     const response = await api.post("reviews", {
         msg: values.msg,
         stars: values.stars,
         product_id: values.productId,
-        user_id: user_id,
+        user_id: values.userId,
         user_role: 1
     },
     {
