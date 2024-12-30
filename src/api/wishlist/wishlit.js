7349bc48e98bfdde205be536c6f62cd0a90c516a
ayaFarh <farhaya004@gmail.com>
2024-12-31 00:28:16 +0200

    update wishlist

--- src/api/wishlist/wishlit.js
+++ src/api/wishlist/wishlit.js
@@ -1,8 +1,8 @@
 import api, { handleError } from "../axiosConfig";
 
-const getAllwishlist = async (id) => {
+const getAllwishlist = async () => {
   try {
-    const response = await api.get(`favorites/${id}`, {
+    const response = await api.get('favorites', {
       headers: {
         Authorization: `Bearer ${localStorage.getItem("token")}`,
       },
