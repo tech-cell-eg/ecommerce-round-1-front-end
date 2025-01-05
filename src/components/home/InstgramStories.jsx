83ca4d9e4150298762c4df7967e5bfd4cae09080
marwa <meroblue25@gmail.com>
2025-01-05 22:25:40 +0200

    fix responsive issue in insta stories in  index

--- src/components/home/InstgramStories.jsx
+++ src/components/home/InstgramStories.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { IoCardOutline } from "react-icons/io5";
 import { PiHeadphonesLight } from "react-icons/pi";
 import { CiDollar } from "react-icons/ci";
@@ -9,60 +9,71 @@ import { fetchAllinstgramstories } from "../../api/instgramStory/getAllinstgrams
 
 function InstgramStories() {
   const [instaStory, setInstaStory] = useState([]);
+  const phrases = [
+    {
+      icon: <BsBoxSeam size={40} />,
+      title: "Free Shipping",
+      description: "Free shipping for the order above $150",
+    },
+    {
+      icon: <CiDollar size={40} />,
+      title: "Money Guarantee",
+      description: "Within 30 days of exchange",
+    },
+    {
+      icon: <PiHeadphonesLight size={40} />,
+      title: "Online Support",
+      description: "24 hours a day, 7 days a week",
+    },
+    {
+      icon: <IoCardOutline size={40} />,
+      title: "Flexible Payment",
+      description: "Pay with multiple credit cards",
+    },
+  ];
+
   useEffect(() => {
-      const getinstastory = async () => {
-        const allinstastory = await fetchAllinstgramstories();
-        setInstaStory(allinstastory)
-      };
-      getinstastory()
-    }, []);
+    const getinstastory = async () => {
+      const allinstastory = await fetchAllinstgramstories();
+      setInstaStory(allinstastory);
+    };
+    getinstastory();
+  }, []);
+
   return (
     <section className="container-main bg-white my-3 py-20">
       <h1 className="capitalize text-center text-4xl font-medium">
         our instgram stories
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 mt-24">
-      {instaStory.map((story) => (
-            <div className="flex flex-col gap-4 justify-center" key={story.id}>
-              <div key={story.id} className="md:h-[360px]  relative" >
-              <img src={story.image_link} alt="" className="w-full h-full object-cover"/>
-              <div className="w-full h-full bg-black/0 hover:bg-black/5 opacity-0 hover:opacity-100 transition-all duration-300  absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  flex justify-center items-center ">
-            <Link to={story.insta_link} className=" text-lg text-center bg-white p-2 w-fit h-fit  rounded-full"><FaInstagram className="text-black" /></Link>
+        {instaStory.map((story, index) => (
+          <div className="flex flex-col gap-4 justify-center" key={story.id}>
+            <div className="md:h-[360px] relative">
+              <img
+                src={story.image_link}
+                alt=""
+                className="w-full h-full object-cover"
+              />
+              <div className="w-full h-full bg-black/0 hover:bg-black/5 opacity-0 hover:opacity-100 transition-all duration-300 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex justify-center items-center">
+                <Link
+                  to={story.insta_link}
+                  className="text-lg text-center bg-white p-2 w-fit h-fit rounded-full"
+                >
+                  <FaInstagram className="text-black" />
+                </Link>
+              </div>
             </div>
+            <div className="flex flex-col gap-4 items-center">
+              <div>{phrases[index % phrases.length].icon}</div>
+              <h4 className="text-xl font-medium">
+                {phrases[index % phrases.length].title}
+              </h4>
+              <p className="text-center text-gray-600">
+                {phrases[index % phrases.length].description}
+              </p>
             </div>
-           
-            </div>
-          ))}
-      </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mt-12">
-        <div className="col-span-1 px-6 flex flex-col gap-4">
-          <div>
-            <BsBoxSeam size={40} />
           </div>
-          <h4 className="text-xl font-medium">Free Shipping</h4>
-          <p>Free shipping for the order above $150</p>
-        </div>
-        <div className="col-span-1 px-6 flex flex-col gap-4">
-          <div>
-            <CiDollar size={40} />
-          </div>
-          <h4 className="text-xl font-medium">Money Guarantee</h4>
-          <p>Within 30 days of exchange</p>
-        </div>
-        <div className="col-span-1 px-6 flex flex-col gap-4">
-          <div>
-            <PiHeadphonesLight size={40} />
-          </div>
-          <h4 className="text-xl font-medium">Online Support</h4>
-          <p>24 hours a day, 7 days a week</p>
-        </div>
-        <div className="col-span-1 px-6 flex flex-col gap-4">
-          <div>
-            <IoCardOutline size={40} />
-          </div>
-          <h4 className="text-xl font-medium">Flexible Payment</h4>
-          <p>Pay with multiple credit cards</p>
-        </div>
+        ))}
       </div>
     </section>
   );
